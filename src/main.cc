d20358d76106deed6e80dec8acc0cc12123021dc
Marco <marcoaremail@gmail.com>
2016-06-26 02:37:28 -0300

    Input file read, using opencv to output images

--- src/main.cc
+++ src/main.cc
@@ -1,58 +1,73 @@
 #include <memory>
+#include <fstream>
 #include <iostream>
-#include "spatial/spatial.h"
-#include "ppm.h"
+#include <sstream>
+#include <opencv2/opencv.hpp>
+#include "graphics/graphics.h"
+#include "filemanip.h"
+
+// [ noise ] (const Geometry::Vec<2> &param) -> Pigment::Color {
+//
+//     // return { (1 + std::sin(param[0])) * 0.5, (1.0 + std::sin(param[0])) * 0.5, (1 + std::sin(param[0])) * 0.5 };
+//
+//     float_max_t
+//         s = std::fmod(param[0], 500.0) + 500.0,
+//         t = std::fmod(param[1], 500.0) + 500.0,
+//         value = (1.0 + std::sin((s + noise.at(s * 5.0, t * 5.0, 0.0) * 0.5) * 50.0)) * 0.5;
+//
+//     // return { noise.at(s, t), noise.at(s, t), noise.at(s, t) };
+//
+//     // return { value, value, value };
+//
+//     s = Geometry::fract(param[0]), t = Geometry::fract(param[1]);
+//     if ((0.49 < s && s < 0.51) || (0.49 < t && t < 0.51)) {
+//         return { 0.5, 0.5, 0.5 };
+//     }
+//     bool right = std::round(s) > 0.5, top = std::round(t) > 0.5;
+//
+//     if ((top && right) || !(top || right)) {
+//         return { 1.0, 1.0, 1.0 };
+//     }
+//     return { 0.0, 0.0, 0.0 };
+// }
 
 int main (int argc, const char *argv[]) {
 
-    const Spatial::Vec<3>
-        eye_pos = { 0.0, 0.0, 5.0 },
-        look_at = { 0.0, 0.0, 0.0 },
-        up_dir = { 0.0, 1.0, 0.0 };
-    const float_max_t fov = 90.0 * Spatial::DEG2RAD;
     const unsigned
         image_width = 800,
         image_height = 600;
     const bool use_projection = true;
 
-    std::vector<std::unique_ptr<Spatial::Shape>> shapes;
+    Geometry::Camera camera;
+    Pigment::Color ambient;
+    std::vector<Light::Light *> lights;
+    std::vector<Pigment::Texture *> pigments;
+    std::vector<Light::Surface *> surfaces;
+    std::vector<Shape::Shape *> shapes;
 
-    // transf->shear(1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-    // transf->rotate( Spatial::Quaternion::axisAngle(Spatial::Vec<3>::axisZ, Spatial::DEG45) );
-    // transf->scale( 1.0, 2.0, 1.0 );
-    // transf->translate( -1.0, -1.0, -1.0 );
-
-    shapes.push_back(std::make_unique<Spatial::CSGTree>(
-        Spatial::CSGTree(new Spatial::Sphere({ 0.0, 0.0, 0.0 }, 1.0), Spatial::CSGTree::SUBTRACTION, new Spatial::Cylinder({ 1.0, 0.0, -2.0 }, { 1.0, 0.0, 2.0 }, 1.0))
-    ));
-    // shapes.push_back(std::make_unique<Spatial::Box>(Spatial::Box({ 3.0, 3.0, 3.0 }, { 5.0, 5.0, 5.0 })));
-    // shapes.push_back(std::make_unique<Spatial::Cylinder>(Spatial::Cylinder({ -1.0, -1.0, -0.0 }, { 1.0, 1.0, 0.0 }, 1.0)));
-    // shapes.push_back(std::make_unique<Spatial::Polyhedron>(Spatial::Polyhedron({
-    //     Spatial::Plane({ 0.0, 0.0, 1.0 }, 0.0),
-    //     Spatial::Plane({ 0.0, 1.0, 0.0 }, 0.0),
-    //     Spatial::Plane({ 1.0, 0.0, 0.0 }, 0.0)
-    // })));
-
-    // polyhedrons.push_back({
-    //     std::make_pair(Spatial::Vec<3>({ 0.0, 0.0, -1.0 }), 0.0),
-    //     std::make_pair(Spatial::Vec<3>({ 0.0, 0.0, 0.0 }), 0.0),
-    //     std::make_pair(Spatial::Vec<3>({ 1.0, 1.0, 0.0 }), 0.0)
-    // });
-    //
+    FileManip::readFile(argv[1], camera, ambient, lights, pigments, surfaces, shapes);
 
     const float_max_t
         inv_image_width = 1.0 / static_cast<float_max_t>(image_width),
         inv_image_height = 1.0 / static_cast<float_max_t>(image_height),
         aspect_ratio = static_cast<float_max_t>(image_width) * inv_image_height,
-        scale = std::tan(fov * 0.5);
-    const Spatial::Vec<3>
+        scale = std::tan(camera.getFieldOfView() * 0.5);
+    const Geometry::Vec<3>
+        look_at = camera.getLookAt(),
+        eye_pos = camera.getPosition(),
+        up_dir = camera.getUpDirection(),
         camera_direction = (look_at - eye_pos).normalized(),
         camera_right = camera_direction.cross(up_dir),
         camera_up = camera_right.cross(camera_direction);
 
     std::vector<float_max_t> pixel_x_cache(image_width);
 
-    PPM img(image_width, image_height, PPM::Pixel(0.0, 0.0, 0.0));
+    cv::Mat img = cv::Mat::zeros(image_height, image_width, CV_8UC3);
+
+    float_max_t t_min, t_max;
+    Geometry::Vec<3> normal_min, normal_max;
+    Pigment::Color color_min, color_max;
+    Light::Material material_min, material_max;
 
     for (unsigned pixel_y = 0; pixel_y < image_height; ++pixel_y) {
         const float_max_t world_y = (1.0 - static_cast<float_max_t>((pixel_y << 1) + 1) * inv_image_height) * scale;
@@ -62,29 +77,32 @@ int main (int argc, const char *argv[]) {
                 pixel_x_cache[pixel_x] = (static_cast<float_max_t>((pixel_x << 1) + 1) * inv_image_width - 1.0) * aspect_ratio * scale;
             }
 
-            const Spatial::Vec<3> position =
+            const Geometry::Vec<3> position =
                 pixel_x_cache[pixel_x] * camera_right + world_y * camera_up + eye_pos + camera_direction;
 
-            const Spatial::Line line(position, use_projection ? (position - eye_pos).normalized() : camera_direction);
+            const Geometry::Line line(position, use_projection ? (position - eye_pos).normalized() : camera_direction);
+            float_max_t global_min = std::numeric_limits<float_max_t>::infinity();
 
             for (const auto &shape : shapes) {
 
-                float_max_t t_min, t_max;
-                Spatial::Vec<3> normal_min, normal_max;
-
-                if (shape->intersectLine(line, normal_min, normal_max, t_min, t_max, true)) {
+                if (shape->intersectLine(line, normal_min, normal_max, color_min, color_max, material_min, material_max, t_min, t_max, true)) {
                     if (t_min > 0.0) {
-                        img.setColor({ pixel_x, pixel_y }, PPM::Pixel(t_min, t_min, t_min));
+                        if (t_min < global_min) {
+                            global_min = t_min;
+                            img.at<cv::Vec3b>(pixel_y, pixel_x) = color_min;
+                        }
                     } else if (t_max > 0.0) {
-                        img.setColor({ pixel_x, pixel_y }, PPM::Pixel(t_max, t_max, t_max));
+                        if (t_max < global_min) {
+                            global_min = t_max;
+                            img.at<cv::Vec3b>(pixel_y, pixel_x) = color_max;
+                        }
                     }
                 }
             }
         }
     }
 
-    img.normalize();
-    img.write(argv[1]);
+    cv::imwrite(argv[2], img);
 
     return 0;
 }
