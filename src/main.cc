43d49a542da86648e4a576a86098e3cfed2aa673
Marco <marcoaremail@gmail.com>
2016-06-28 12:12:13 -0300

    Removed unnecessary comment on top

--- src/main.cc
+++ src/main.cc
@@ -7,31 +7,6 @@
 #include "graphics/graphics.h"
 #include "filemanip.h"
 
-// [ noise ] (const Geometry::Vec<2> &param) -> Pigment::Color {
-//
-//     // return { (1 + std::sin(param[0])) * 0.5, (1.0 + std::sin(param[0])) * 0.5, (1 + std::sin(param[0])) * 0.5 };
-//
-//     float_max_t
-//         s = std::fmod(param[0], 500.0) + 500.0,
-//         t = std::fmod(param[1], 500.0) + 500.0,
-//         value = (1.0 + std::sin((s + noise.at(s * 5.0, t * 5.0, 0.0) * 0.5) * 50.0)) * 0.5;
-//
-//     // return { noise.at(s, t), noise.at(s, t), noise.at(s, t) };
-//
-//     // return { value, value, value };
-//
-//     s = Geometry::fract(param[0]), t = Geometry::fract(param[1]);
-//     if ((0.49 < s && s < 0.51) || (0.49 < t && t < 0.51)) {
-//         return { 0.5, 0.5, 0.5 };
-//     }
-//     bool right = std::round(s) > 0.5, top = std::round(t) > 0.5;
-//
-//     if ((top && right) || !(top || right)) {
-//         return { 1.0, 1.0, 1.0 };
-//     }
-//     return { 0.0, 0.0, 0.0 };
-// }
-
 int main (int argc, const char *argv[]) {
 
     const unsigned
